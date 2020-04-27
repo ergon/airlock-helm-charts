d1d550b1887c2d772f9b7927d55806a66e154ec9
Julian Bersnak <58738040+jubeergon@users.noreply.github.com>
2020-04-27 16:58:32 +0200

    Update helm-docs-generate.sh

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -8,11 +8,12 @@ echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" 
 git config user.name "$TECHNICAL_USER"
 git config user.email "$TECHNICAL_USER@users.noreply.github.com"
 
+git checkout  ${BRANCH_NAME}
+
 ./helm-docs
 
 READMES_CHANGED=$(git diff --name-only HEAD -- 'charts/**/README.md')
 
-git checkout  ${BRANCH_NAME}
 for README_CHANGED in ${READMES_CHANGED}; do
   git add ${README_CHANGED}
 done 
