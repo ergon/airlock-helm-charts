1b61f5770768554a4ec5b1230977743b25ad7c42
Stefan Dietiker <StefanDietiker@outlook.com>
2020-04-27 15:16:02 +0200

    Update helm-docs-install.sh

--- .github/helm-docs-install.sh
+++ .github/helm-docs-install.sh
@@ -2,5 +2,5 @@
 set -euox pipefail
 
 # install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"$HELM_DOCS_VERSION"/helm-docs_"$HELM_DOCS_VERSION"_Linux_x86_64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
