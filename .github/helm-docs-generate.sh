1e6eb09c25eb5819b77c6e2b8397085eee61cc0e
Julian Bersnak <58738040+jubeergon@users.noreply.github.com>
2020-04-27 12:58:00 +0200

    Update helm-docs-generate.sh

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -5,8 +5,8 @@ set -euox pipefail
 echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" >&2
 echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
 
-git config user.name "$GITHUB_USER"
-git config user.email "$GITHUB_USER@users.noreply.github.com"
+git config user.name "$TECHNICAL_USER"
+git config user.email "$TECHNICAL_USER@users.noreply.github.com"
           
 ./helm-docs
 
@@ -14,6 +14,6 @@ git checkout ${BRANCH_NAME}
 git add charts/microgateway/README.md
 git commit -m "Automated README generation"
 echo "Push to ${BRANCH_NAME}"
-git push "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY} ${BRANCH_NAME}
+git push "https://$TECHNICAL_USER:$TECHNICAL_USER_TOKEN@github.com/${GITHUB_REPOSITORY} ${BRANCH_NAME}
 
 rm helm-docs
