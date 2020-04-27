71d600aacae66d8433aa5980a6f8c0fd43b378c4
Julian Bersnak <58738040+jubeergon@users.noreply.github.com>
2020-04-27 15:07:40 +0200

    Update helm-docs-generate.sh

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-set -euox pipefail
+set -euo pipefail
 
 # Write Error Message
 echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" >&2
 echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
 
-git config user.name "$TECHNICAL_USER"
-git config user.email "$TECHNICAL_USER@users.noreply.github.com"
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
           
 ./helm-docs
 
-git checkout ${BRANCH_NAME}
-git add charts/microgateway/README.md
+READMES_CHANGED=$(git diff --name-only HEAD -- 'charts/**/README.md')
+
+git checkout  ${BRANCH_NAME}
+for README_CHANGED in ${READMES_CHANGED}; do
+  git add ${README_CHANGED}
+done 
+
 git commit -m "Automated README generation"
 echo "Push to ${BRANCH_NAME}"
-git push "https://$TECHNICAL_USER:$TECHNICAL_USER_TOKEN@github.com/${GITHUB_REPOSITORY}.git" ${BRANCH_NAME}
+git push "https://$GITHUB_ACTOR:$CR_TOKEN@github.com/libuweber/microgateway.git" ${BRANCH_NAME}
 
 rm helm-docs
