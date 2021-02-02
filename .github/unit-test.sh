b40dcfb50cb44e38261fae1a9c52f7a02063ee41
Roland Weber <roland.weber@ergon.ch>
2021-02-02 16:30:20 +0100

    fix unit test script

--- .github/unit-test.sh
+++ .github/unit-test.sh
@@ -2,7 +2,7 @@
 set -euox pipefail
 
 # install unittest helm plugin
-#helm plugin install https://github.com/quintush/helm-unittest
+helm plugin install https://github.com/quintush/helm-unittest
 
 #run unittest
 helm unittest charts/microgateway
