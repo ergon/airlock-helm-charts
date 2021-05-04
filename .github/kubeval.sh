0c2e5930fead5f4d6ba3ca300cdaf3c67e40dd6c
Roland Weber <roland.weber@ergon.ch>
2021-05-04 11:55:38 +0200

    fix kubeval (#112)
    * fix kubeval
    - kubeval: use kubernetes-json-schema repo with current versions
    - kubeval: use latest supported k8s versions
    - kubeval: do not ignore missing schemas
    - kind test: remove unsupported k8s version

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 set -euox pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | cut -f1-2 -d '/' | uniq)"
 KUBEVAL_VERSION="0.15.0"
-SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
@@ -11,5 +11,5 @@ tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  helm template "${CHART_DIR}" | ./kubeval --strict --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
