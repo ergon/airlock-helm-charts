0191928d83759d020d763bbc5b2ca70f7457b6de
Roland Weber <roland.weber@ergon.ch>
2021-05-06 15:09:13 +0200

    simplify readme generation (#114)
    simplify readme generation
    
    * remove doc verify step from workflow
    * use checkout v2
    * use docker image to run helmdoc

--- .github/helm-docs-generate.sh
+++ .github/helm-docs-generate.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 set -euox pipefail
 
-# Write Error Message
-echo -e "\033[0;31m ######## The helm-docs validation failed. ######## \033[0m" >&2
-echo -e "\033[0;31m Make sure that the documentation has been updated. \033[0m" >&2
-
 git config user.name "$TECHNICAL_USER"
 git config user.email "$TECHNICAL_USER@users.noreply.github.com"
-
 git fetch
 git checkout ${BRANCH_NAME}
 
-./helm-docs
+# create readme
+make
 
+# push readme updates
 READMES_CHANGED=$(git diff --name-only HEAD -- 'charts/**/README.md')
 
+change_count=0
 for README_CHANGED in ${READMES_CHANGED}; do
   git add ${README_CHANGED}
-done 
+  ((++change_count))
+done
 
-git commit -m "Automated README generation"
-echo "Push to ${BRANCH_NAME}"
-git push "https://$TECHNICAL_USER:$TECHNICAL_USER_TOKEN@github.com/${GITHUB_REPOSITORY}.git" ${BRANCH_NAME}
-
-git checkout master
-
-rm helm-docs
+if [ $change_count -gt 0 ]; then
+  echo "Push ${change_count} readmes to ${BRANCH_NAME}"
+  git commit -m "Automated README generation"
+  git push "https://$TECHNICAL_USER:$TECHNICAL_USER_TOKEN@github.com/${GITHUB_REPOSITORY}.git" ${BRANCH_NAME}
+fi
